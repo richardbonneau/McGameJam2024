e1f1c8218d5ac764ce66ec176f84aabd5d5e34aa
richardbonneau <richardbonneau91@gmail.com>
2024-01-20 17:49:37 -0500

    camera free look

--- addons/cars/camera/CameraFollow.gd
+++ addons/cars/camera/CameraFollow.gd
@@ -6,8 +6,19 @@ extends Camera3D
 @export var speed:=20.0
 var follow_this = null
 var last_lookat
+var is_panning:bool=false 
+
+@onready var left_cam = get_parent().get_node("LeftCam")
+@onready var right_cam = get_parent().get_node("RightCam")
+@onready var behind_cam = get_parent().get_node("BehindCam")
+@onready var car_base = get_parent().get_parent()
+@onready var player = car_base.get_parent()
+
+@onready var player_num:String = str(player.player_index + 1)
+
 
 func _ready():
+	print(player_num)
 	follow_this = get_parent()
 	last_lookat = follow_this.global_transform.origin
 
@@ -25,8 +36,23 @@ func _physics_process(delta):
 	else:
 		target_pos.y = follow_this.global_transform.origin.y + target_height
 	
-	global_transform.origin = global_transform.origin.lerp(target_pos, delta * speed)
+	if Input.get_action_strength("player"+player_num+"_lookleft"): 
+		is_panning = true
+		left_cam.current = true
+	elif  Input.get_action_strength("player"+player_num+"_lookright"):
+		is_panning = true
+		right_cam.current = true
+	elif Input.get_action_strength("player"+player_num+"_lookdown"):
+		is_panning = true
+		behind_cam.current = true
+	else: 
+		is_panning = false
+		self.current = true
 	
+	
+	global_transform.origin = global_transform.origin.lerp(target_pos, delta * speed)
 	last_lookat = last_lookat.lerp(follow_this.global_transform.origin, delta * speed)
 	
-	look_at(last_lookat, Vector3.UP)
+	if not is_panning: 
+		look_at(last_lookat, Vector3.UP)
+
